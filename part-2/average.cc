d21a2d6b3f86f78a1ae3104fbae98b41c2d528ca
Ramon <chrisramon1@cs300u-6c.acad.fullerton.edu>
2023-10-30 16:00:47 -0700

    done

--- part-2/average.cc
+++ part-2/average.cc
@@ -10,55 +10,39 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
+  // Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
   if (arguments.size() < 2 ) {
-    std::cout << "you must supply at least one number\n ";
+    std::cout << "error: you must supply at least one number\n ";
   return 1;
   }
-  // TODO: Write a for-each loop to sum (add up) all of the command line
+  // Write a for-each loop to sum (add up) all of the command line
   // arguments.
-  //bool skip = true;
-  //for(std::string i: arguments){
-    //if (skip == true ) { 
-    //  skip = false;
-     // continue;
-    //}
-    //else {
-      
-    //}
-  //}
-double sum {0.0}
-for (int i= 1; i < arguments.size[i]; i++){
-   sum += num
-}
-
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  double number = std::stod(arguments[1]);
-  sum += number;
-
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
+  double sum {0.0};
+  
   bool skip = true;
-  for (std::string i: arguments) {
-     if (skip == true ) {
+  for(const std::string &element: arguments){
+    if(skip == true){
       skip = false;
       continue;
-     }
-     else {
-      static_cast<double> 
-     }
+    } 
+    sum += std::stod(element);  
   }
-  // TODO: After the loop has finished summing the arguments, calculate the
+  // for (long i = 1; i < arguments.size(); i++) {
+  //    double number = std::stod(arguments[i]);
+  //    if (number != 0.0) {
+  //     sum += number;
+  //     total++;
+  //    }
+  // }
+  double average = sum / (static_cast<int> (arguments.size() - 1));
+  // After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
-
-  // TODO: Use cout to print out a message of the form
+  // Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
-
+  std::cout << "average" 
+            << " = " << average << "\n";
   return 0;
 }
