f84c9fb32d2a57968cbda196576c60a53e85dff1
Ramon <chrisramon1@cs300u-6c.acad.fullerton.edu>
2023-10-30 15:10:16 -0700

    hdbah

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Chris Ramon
+// chrisramon1@csu.fullerton.edu
+// @smoothchris1
+// Partners: @Miguel16037
 
 #include <iostream>
 #include <string>
@@ -9,16 +12,46 @@ int main(int argc, char* argv[]) {
 
   // TODO: Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
-
+  if (arguments.size() < 2 ) {
+    std::cout << "you must supply at least one number\n ";
+  return 1;
+  }
   // TODO: Write a for-each loop to sum (add up) all of the command line
   // arguments.
+  //bool skip = true;
+  //for(std::string i: arguments){
+    //if (skip == true ) { 
+    //  skip = false;
+     // continue;
+    //}
+    //else {
+      
+    //}
+  //}
+double sum {0.0}
+for (int i= 1; i < arguments.size[i]; i++){
+   sum += num
+}
+
   // Use a double or float type so that your program preserves fractional
   // values.
+  double number = std::stod(arguments[1]);
+  sum += number;
+
   // The loop needs to skip over the command name, which is the first element
   // of the arguments vector.
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
-
+  bool skip = true;
+  for (std::string i: arguments) {
+     if (skip == true ) {
+      skip = false;
+      continue;
+     }
+     else {
+      static_cast<double> 
+     }
+  }
   // TODO: After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
